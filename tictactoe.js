60caf3fe1428a8cb29a460d19449db19a2c643fd
Daniel A Ramirez <dr82913@srcs.loc>
2018-04-19 12:18:32 -0700

    Fixing null error

--- tictactoe.js
+++ tictactoe.js
@@ -1,6 +1,7 @@
 var block1 = document.getElementById("blockOne");
+if (block1){
 block1.addEventListener("click", addXO);
-
+}
 function addXO() {
     document.getElementById('result').textContent = "4";
 }
