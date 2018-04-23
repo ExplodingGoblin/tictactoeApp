61cb8916eeda89976323b92f1bad05470e06ab71
Maxwell L Rahe <mr03989@srcs.loc>
2018-04-23 11:26:46 -0700

    new function

--- tictactoe.js
+++ tictactoe.js
@@ -1,10 +1,18 @@
 var block1 = document.getElementById("blockOne");
 block1.addEventListener("click", addXO);
 
+var player1Points = 0;
+var player2Points = 0;
+
+
+
 function addXO() {
     document.getElementById('result').textContent = "4";
 }
 
+function newGame() {
 
 
+winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
+}
