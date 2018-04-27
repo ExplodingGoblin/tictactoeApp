3b345101bdd8096565d1cda1d6c474e7fc7597e9
Daniel A Ramirez <dr82913@srcs.loc>
2018-04-27 10:46:59 -0700

    Made new code

--- tictactoe.js
+++ tictactoe.js
@@ -1,18 +1,51 @@
-var block1 = document.getElementById("blockOne");
-if (block1) {
-    block1.addEventListener("click", addXO);
+var player1Choices = new Array();
+var player2Choices = new Array();
+var numberOfPlayers = 2;
+var currentPlayer = 0;
+var turn = 0;
+var points1 = 0;    // player 1 points
+var points2 = 0;    // player 2 points
+
+function drawBoard() {
+    var Parent = document.getElementById("game");
+    var turnCount = 1;
+
+    while (Parent.hasChildNodes()) {
+        Parent.removeChild(Parent.firstChild);
+    }
+
+    for (s = 0; s < 3; s++) {
+        var row = document.createElement("tr");
+
+        for (r = 0; r < 3; r++) {
+            var col = document.createElement("td");
+            col.id = counter;
+            col.innerHTML = counter;
+
+            var handler = function(e) {
+                if (currentPlayer == 0) {
+                    this.innerHTML = "X";
+                    player1Selections.push(parseInt(this.id));
+                    player1Selections.sort(function(a, b) { return a - b });
+                }
+
+                else {
+                    this.innerHTML = "O";
+                    player2Selections.push(parseInt(this.id));
+                    player2Selections.sort(function(a, b) { return a - b });
+                }
+
+                move++;
+            };
+
+            col.addEventListener('click', handler);
+
+            row.appendChild(col);
+            counter++;
+        }
+
+        Parent.appendChild(row);
+    }
 }
 
-var player1Points = 0;
-var player2Points = 0;
-
-function addXO() {
-    document.getElementById('result').textContent = "4";
-}
-
-function newGame() {
-
-
-    winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-
-}
+window.onload = drawBoard;
--- tictactoe_Daniel.js
+++
@@ -1,8 +0,0 @@
-var block1 = document.getElementById("blockOne");
-if (block1){
-    block1.addEventListener("click", itWorks);
-}
-
-function itWorks(){
-    console.log(5 + 6);
-}
