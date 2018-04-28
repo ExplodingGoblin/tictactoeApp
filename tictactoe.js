4e5b10b63eea526873ba9021a7cd19763322288c
Richard Rahe <rickyfingers@Richards-Mini-2.hsd1.ca.comcast.net>
2018-04-28 15:01:24 -0700

    Win Conditions, reset

--- tictactoe.js
+++ tictactoe.js
@@ -3,8 +3,9 @@ var player2Choices = new Array();
 var numberOfPlayers = 2;
 var currentPlayer = 0;
 var turn = 0;
-var points1 = 0;    // player 1 points
-var points2 = 0;    // player 2 points
+var points1 = 0; // player 1 points
+var points2 = 0; // player 2 points
+var winCondition;
 
 function drawBoard() {
     var Parent = document.getElementById("game");
@@ -13,10 +14,10 @@ function drawBoard() {
         Parent.removeChild(Parent.firstChild);
     }
 
-    for (s = 0; s < 3; s++) {
+    for (r = 0; r < 3; r++) {
         var row = document.createElement("tr");
 
-        for (r = 0; r < 3; r++) {
+        for (c = 0; c < 3; c++) {
             var col = document.createElement("td");
             row.appendChild(col);
         }
@@ -26,3 +27,21 @@ function drawBoard() {
 }
 
 window.onload = drawBoard;
+
+
+function win() {
+    winCondition.push([1, 2, 3]);
+    winCondition.push([4, 5, 6]);
+    winCondition.push([7, 8, 9]);
+    winCondition.push([1, 4, 7]);
+    winCondition.push([2, 5, 8]);
+    winCondition.push([3, 6, 9]);
+    winCondition.push([1, 5, 9]);
+    winCondition.push([3, 5, 7]);
+}
+
+function restart() {
+    currentPlayer = 0;
+    player1Choices = new Array;
+    player2Choices = new Array;
+}
